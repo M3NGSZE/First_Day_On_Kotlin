4e1592d91dced25f0cb26b116f96705b68c25cf9
M3NGSZE <mengse.thun13@gmail.com>
2025-07-24 22:37:38 +0700

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,7 @@
+package com.mengse.java
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    println("Hello my name is Mengse")
+}
